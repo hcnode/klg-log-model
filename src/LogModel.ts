ba72d925f1aeb6eba24926263a97a4a1d55b45c3
Nick <myfjdthink@163.com>
2018-11-02 09:54:23 +0800

    interfaceName add index

--- src/LogModel.ts
+++ src/LogModel.ts
@@ -6,7 +6,7 @@ export const modelSchema = {
   type: {type: String, required: true, enum: ['in', 'out'], index: true},  // 类型，是请求其他系统 out 还是接受其他请求 in
   server: {type: String},   // 请求到哪里，只有 out 的 log 需要记录
   useTime: {type: Number},  // 请求耗时 ms
-  interfaceName: {type: String},    // 接口名
+  interfaceName: {type: String, index: true},    // 接口名
   httpMethod: {type: String},   // get or  post
   retryTimes: {type: Number},   // 重试次数, 对外请求会因为网络问题需要重试，记录该字段可以帮助开发判断网络情况
   url: {type: String, index: true},          // 请求 url
